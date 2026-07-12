55be8264b6913eb457c56bb53ca58210de2bb7c8
NamanGangwani <NamanGangwani@outlook.com>
2026-07-11 19:07:08 -0500

    added score and lives + UI for them

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal died
+
 @export var speed = 250
 
 func _ready() -> void:
@@ -9,4 +11,9 @@ func _physics_process(delta: float) -> void:
 	global_position.x -= speed * delta
 
 func die() -> void:
+	emit_signal("died")
 	queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	body.take_damage()
+	die()
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,6 +1,29 @@
 extends Node2D
 
+var lives = 3
+var score = 0
+
+@onready var player = $Player
+@onready var hud = $UI/HUD
+
+func _ready() -> void:
+	hud.set_score_label(score)
+	hud.set_lives(lives)
 
 func _on_deathzone_area_entered(area: Area2D) -> void:
 	print("deleted enemy")
 	area.die()
+
+func _on_player_took_damage() -> void:
+	lives -= 1
+	hud.set_lives(lives)
+	if lives == 0:
+		player.die()
+
+func _on_enemy_spawner_enemy_spawned(enemy_instance: Variant) -> void:
+	enemy_instance.connect("died", _on_enemy_died)
+	add_child(enemy_instance)
+
+func _on_enemy_died() -> void:
+	score += 100
+	hud.set_score_label(score)
